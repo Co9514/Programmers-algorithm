28407e74b94d0d32d3a198eb4612b3cc5bd7e2b7
Co9514 <khj950104@naver.com>
2020-05-11 21:02:15 +0900

    Title : 최대값과 최소값 Level : 2 문제 풀이 : 2레벨 문제라고는 보기 힘들었다. 1레벨 수준인 것 처럼 느껴졌다. 단순히 최대값과 최소값들을 순차적으로 비교하면 된다. sort로 정렬해서 첫번쨰와 마지막을 뽑는 것도 될지 해봐야겠다.

---
+++ src/level2/MaxAndMin.java
@@ -0,0 +1,28 @@
+package level2;
+
+/*
+문제 설명
+문자열 s에는 공백으로 구분된 숫자들이 저장되어 있습니다. str에 나타나는 숫자 중 최소값과 최대값을 찾아 이를 (최소값) (최대값)형태의 문자열을 반환하는 함수, solution을 완성하세요.
+예를들어 s가 1 2 3 4라면 1 4를 리턴하고, -1 -2 -3 -4라면 -4 -1을 리턴하면 됩니다.
+
+제한 조건
+s에는 둘 이상의 정수가 공백으로 구분되어 있습니다.
+ */
+
+public class MaxAndMin {
+    public String solution(String s){
+        String[] x = s.split(" ");
+        int max = Integer.parseInt(x[0]);
+        int min = Integer.parseInt(x[0]);
+        for (int i = 1; i < x.length ; i++) {
+            max = Integer.max(max,Integer.parseInt(x[i]));
+            min = Integer.min(min,Integer.parseInt(x[i]));
+        }
+        return min + " " + max ;
+    }
+
+    public static void main(String[] args) {
+        MaxAndMin maxAndMin = new MaxAndMin();
+        System.out.println(maxAndMin.solution("-1 -2 -3 -4"));
+    }
+}
