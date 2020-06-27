3bf67e15a6a6fb883e432db94c8f2d3a7afefc25
Kim Hyeongjin <khj950104@naver.com>
2020-06-27 22:14:51 +0900

    Title : 정수 제곱근 판별

---
+++ src/level1/SquareRoot.java
@@ -0,0 +1,15 @@
+package level1;
+/*
+문제 설명
+임의의 양의 정수 n에 대해, n이 어떤 양의 정수 x의 제곱인지 아닌지 판단하려 합니다.
+n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고, n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요.
+
+제한 사항
+n은 1이상, 50000000000000 이하인 양의 정수입니다.
+*/
+class SquareRoot {
+  public long solution(long n) {
+        double d = (int)Math.sqrt(n);
+        return n ==  Math.pow(d,2) ? (long)Math.pow(d + 1 ,2) : -1;
+  }
+}
